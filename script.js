78a5cfef59f37b26992f5f180a4b9b88675d2bc6
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-12 12:27:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function fibonacci(num) {
-// your code here
-	let a=0;
-	 let b=1;
-	let result=[];
-	for(let i=0;i<num ; i++){
-		result.push(a);
-		let next =a+b;
-		a=b;
-		b=next;
-	}
-	return result.join(",");
+    let a = 0;
+    let b = 1;
+    let result;
+
+    for (let i = 0; i < num; i++) {
+        result = a;
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return result;
 }
 
 module.exports = fibonacci;
